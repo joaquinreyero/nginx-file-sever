3239c3c5a223d3fe9f812513c23f9bd7680c4151
joaquinreyero <joaquinreyero@hotmail.com>
2024-06-14 21:58:48 -0300

    testing jinja2 templates

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,14 +1,20 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.templating import Jinja2Templates
 import os
 
 app = FastAPI()
 
 UPLOAD_DIRECTORY = "/usr/share/nginx/html/files"
+templates = Jinja2Templates(directory="templates")
 
 # Asegúrate de que el directorio de subida existe
 if not os.path.exists(UPLOAD_DIRECTORY):
     os.makedirs(UPLOAD_DIRECTORY)
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("upload.html", {"request": request})
 
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
@@ -16,6 +22,11 @@ async def upload_file(file: UploadFile = File(...)):
     with open(file_location, "wb") as buffer:
         buffer.write(file.file.read())
     return {"info": f"file '{file.filename}' saved at '{file_location}'"}
+
+@app.get("/files", response_class=HTMLResponse)
+async def list_files(request: Request):
+    files = os.listdir(UPLOAD_DIRECTORY)
+    return templates.TemplateResponse("files.html", {"request": request, "files": files})
 
 @app.get("/files/{filename}")
 async def get_file(filename: str):
